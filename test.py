535677a99f4effdca2b36e9e9876a9b9180f87b3
Paloma Araujo <39506102+paclaraujo@users.noreply.github.com>
2021-10-13 20:35:46 -0300

    test: add unit test

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-                                                                                                                                                                                  
+from app import app                                                                                                                                                                                      
+import unittest                                                                                                                                                                                          
+
+class Test(unittest.TestCase):                                                                                                                                                                           
+
+    def setUp(self):                                                                                                                                                                                                                                                                                                                            
+        self.app = app.test_client()                                                                                                                                                                     
+                                                                                                                                                       
+        self.result = self.app.get('/')                                                                                                                                                                  
+
+    def test_requisicao(self):                                                                                                                                                                                                                                                                                                                
+        self.assertEqual(self.result.status_code, 200)                                                                                                                                                   
+
+    def test_conteudo(self):                                                                                                                                                                                                                                                                                                                                  
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
