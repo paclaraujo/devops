d116120faae9b1787c63c0c9467840a044f7e895
Paloma Araujo <39506102+paclaraujo@users.noreply.github.com>
2021-11-03 20:14:11 -0300

    chore: add bug to validate sonar

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Devops Lab"
+    return "Devops Lab
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
 
 if __name__ == '__main__':
     app.run(debug=True)
